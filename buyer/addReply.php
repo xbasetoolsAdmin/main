c106d91ed5c97824055f84f1475cb94fc5c395e7
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-04 07:15:48 -0700

    Update addReply.php

--- buyer/addReply.php
+++ buyer/addReply.php
@@ -10,12 +10,17 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
+
+
 <?php
 $uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 $rep = mysqli_real_escape_string($dbcon, $_POST['Reply']);
 $id = mysqli_real_escape_string($dbcon, $_GET['id']);
-  $msg     = '
-  <div class="panel panel-default"><div class="panel-body"><div class="ticket">'.htmlspecialchars($rep).'</div></div><div class="panel-footer"><div class="label label-info">' . $uid . '</div> - '.date("d/m/Y h:i:s a").'</div></div>
+  $msg     = '  <div class="panel panel-default">
+  <div class="panel-body"><div class="ticket">'.htmlspecialchars($rep).'</div>
+  </div>
+  <div class="panel-footer">
+  <div class="label label-info">' . $uid . '</div> - '.date("d/m/Y h:i:s a").'</div></div>
   ';
   if(empty($rep)) { echo "01"; } else {
     $s = mysqli_query($dbcon, "SELECT * FROM ticket WHERE id='$id' AND uid='$uid'") or die();

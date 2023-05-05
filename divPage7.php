14d36b934e7c65879615b4cf5cc432779a8352f9
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-05 21:53:12 +0100

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -11,170 +11,71 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
 
-<ul class="nav nav-tabs">
-  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
-</ul>
-<div id="myTabContent" class="tab-content" >
-  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
-<th>Domain TLD</th>
-<th>Detected Hosting</th>
-<th>Seller</th>
-<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="cpanel_country"><option value="">ALL</option>
+<table width="100%" id="dataTable" class="table table-striped table-bordered table-condensed sticky-header dataTable no-footer" role="grid" aria-describedby="dataTable_info" style="width: 100%;">        <thead>
+  <thead>
+  <tr>
+  <th>ID</th>
+  <th>Type</th>
+  <th>Country</th>
+  <th>Site Name</th>
+  <th>Information</th>
+  <th>Open</th>
+  <th>Date added</th>
+  <th>Price</th>
+  <th>Action</th>
+  </tr>
+        </thead>
+		 <tbody id='tbody2'>
+ <?php
+$uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+$qu = mysqli_query($dbcon, "SELECT * FROM accounts WHERE acctype='account' AND resseller='$uid' ORDER BY id DESC")or die(mysqli_error());
+
+ while($row = mysqli_fetch_assoc($qu)){
+	 
+    echo "<tr class='accounts-tabel'>
+    <td> ".htmlspecialchars($row['id'])." </td>
+    <td> ".strtoupper(htmlspecialchars($row['acctype']))." </td>
+    <td> ".htmlspecialchars($row['country'])." </td>
+    <td> ".htmlspecialchars($row['sitename'])." </td>
+    <td> ".htmlspecialchars($row['infos'])." </td>
+	<td>  "; ?>
+	<a data-toggle="modal" class="btn btn-primary btn-xs" data-target="#myModald<?php echo  $row['id']; ?>" >
+<font color=white>Open #<? echo htmlspecialchars($row['id']); ?> </a></center> 
 <?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `cpanels` WHERE `sold` = '0' ORDER BY country ASC");
-	while($row = mysqli_fetch_assoc($query)){
-	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
-	}
-?>
-</select></td><td><input class='filterinput form-control input-sm' name="cpanel_tld" size='3'></td><td><input class='filterinput form-control input-sm' name="cpanel_hosting" size='3'></td><td><select class='filterselect form-control input-sm' name="cpanel_seller"><option value="">ALL</option>
-<?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `cpanels` WHERE `sold` = '0' ORDER BY resseller ASC");
-	while($row = mysqli_fetch_assoc($query)){
-		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-	}
-?>
-</select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
-</div>
-
-
-<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
-<thead>
-    <tr>
-      <th scope="col" >Country</th>
-      <th scope="col">TLD</th>
-      <th scope="col">Detect Hosting</th>
-      <th scope="col">Seller</th>
-      <th scope="col">Check</th>
-      <th scope="col">Price</th>
-      <th scope="col">Added on </th>
-
-      <th scope="col">Buy</th>
-    </tr>
-</thead>
-  <tbody>
-<?php
-		include("cr.php");
-	    $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysql_error());
-	   	function srl($item)
-		{
-		$item0 = $item;
-		$item1 = rtrim($item0);
-		$item2 = ltrim($item1);
-		return $item2;
-		} 
-
- while($row = mysqli_fetch_assoc($q)){
-	 	 $countryfullname = $row['country'];
-	  $code = array_search("$countryfullname", $countrycodes);
-	 $countrycode = strtolower($code);
-
-	    $url = $row['url'];
-	 	$d = explode("|", $url);
-		$urled = srl($d[0]);
-     $tld = end(explode(".",    
-     parse_url(
-     $urled, PHP_URL_HOST))
-   ); 
-    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
-
-while($rpw = mysqli_fetch_assoc($qer))
-
-$SellerNick = "seller".$rpw["id"]."";
-     echo "
- <tr>    
-    <td id='cpanel_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
-		    <td id='cpanel_tld'> .".$tld." </td>
-    <td id='cpanel_hosting'> ".htmlspecialchars($row['infos'])." </td>
-    <td id='cpanel_seller'> ".htmlspecialchars($SellerNick)."</td>
-";
-	 echo '<td><span id="shop'.$row["id"].'" type="cpanel"><a onclick="javascript:check('.$row["id"].');" class="btn btn-info btn-xs"><font color=white>Check</font></a></span><center></td>';
+  echo '
+ 
+<div class="modal fade" id="myModald' . $row['id'] . '" >
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="myModalLabel">
+                                           <font color="black"> Premium/Shop/Dating #' . $row['id'] . ' </font>
+                                            </h4>
+                                        </div>
+                                        <div class="modal-body">
+					<font color="black">			'.htmlspecialchars($row['url']).' </font>
+					</div>								
+					<div class="modal-footer">
+<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+</div>';
 echo "
+</td>
+    <td> ".htmlspecialchars($row['date'])." </td>
     <td> ".htmlspecialchars($row['price'])."</td>
-	    <td> ".htmlspecialchars($row['date'])."</td>
-    ";
-
-    echo '
-    <td>
-	<span id="cpanel'.$row['id'].'" title="buy" type="cpanel"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
-    </td>
-            </tr>
-     ';
+    <td> ";
+if ($row['sold'] == "0") {
+ echo '<div id="shop'.$row["id"].'" type="delete"><a onclick="javascript:delet('.$row["id"].');" class="btn btn-danger btn-xs">remove</a></div>';
+ }elseif ($row['sold'] == "deleted") {
+	echo "<font color=gray>Deleted</font>"; } else {
+echo "<font color=green>[Sold]</font>";	    
+	}
+    echo "</td>
+    </tr>";
  }
+
+ 
 
  ?>
 
  </tbody>
  </table>
-
-<script type="text/javascript">
-$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#cpanel_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#cpanel_tld").text().toLowerCase() );var ck3 = $.trim( $(this).find("#cpanel_hosting").text().toLowerCase() );var ck4 = $.trim( $(this).find("#cpanel_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="cpanel_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="cpanel_tld"]').val().toLowerCase() );var val3 = $.trim( $('input[name="cpanel_hosting"]').val().toLowerCase() );var val4 = $.trim( $('select[name="cpanel_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || ck3.indexOf(val3)==-1 || (ck4 != val4 && val4 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
-function buythistool(id){
-  bootbox.confirm("Are you sure?", function(result) {
-        if(result ==true){
-      $.ajax({
-     method:"GET",
-     url:"buytool.php?id="+id+"&t=cpanels",
-     dataType:"text",
-     success:function(data){
-         if(data.match(/<button/)){
-		 $("#cpanel"+id).html(data).show();
-         }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-         }
-     },
-   });
-       ;}
-  });
-}
-g:xcheck=0;
-function check(id){   
-     if(xcheck > 1){
-    bootbox.alert("<b>Wait</b> - Other checking operation is executed!");
-  } else {
-    xcheck++;
-    var type = $("#shop"+id).attr('type')
-	$("#shop"+id).html('Checking...').show();
-	$.ajax({
-	type: 		'GET',
-	url: 		'CheckCpanel'+id+'.html',
-	success:	function(data)
-	{
-		$("#shop"+id).html(data).show();
-		xcheck--;
-	}});
-} }
-
-function openitem(order){
-  $("#myModalLabel").text('Order #'+order);
-  $('#myModal').modal('show');
-  $.ajax({
-    type:       'GET',
-    url:        'showOrder'+order+'.html',
-    success:    function(data)
-    {
-        $("#modelbody").html(data).show();
-    }});
-
-}
-
-</script>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body" id="modelbody">
-
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>

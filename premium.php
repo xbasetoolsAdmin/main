a1951b2d18cb112b320fa7b9f1f62bd5381149fe
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-05 12:29:48 +0100

    Update premium.php

--- premium.php
+++ premium.php
@@ -1,175 +1,58 @@
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<link href="//datatables.net/download/build/nightly/jquery.dataTables.css" rel="stylesheet" type="text/css" />
-  
-		<script src="https://code.jquery.com/jquery-3.6.3.js"></script>
-		<script src="//datatables.net/download/build/nightly/jquery.dataTables.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-		<meta charset=utf-8 />
-		<title>DataTables - JS Bin</title>
-	</head>
-	<style>
-	body {
-	font: 90%/1.45em "Helvetica Neue", HelveticaNeue, Verdana, Arial, Helvetica, sans-serif;
-	margin: 0;
-	padding: 0;
-	color: #333;
-	background-color: #fff;
-}
-	
-	</style>
-	<body>
-
-<?php
-ob_start();
-session_start();
-date_default_timezone_set('UTC');
-include "includes/config.php";
-
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-    header("location: ../");
-    exit();
-}
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+<<?php
+include "header.php";
 ?>
-
-<ul class="nav nav-tabs">
-  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
-</ul>
-<div id="myTabContent" class="tab-content" >
-  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
-<th>Site Name</th>
-<th>Seller</th>
-<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="account_country"><option value="">ALL</option>
+<div class="alert alert-danger fade in radius-bordered alert-shadowed"><b>Search for user</b></div>
 <?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `accounts` WHERE `sold` = '0' ORDER BY country ASC");
-	while($row = mysqli_fetch_assoc($query)){
-	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
-	}
-?>
-</select></td><td><input class='filterinput form-control input-sm' name="account_sitename" size='3'></td><td><select class='filterselect form-control input-sm' name="account_seller"><option value="">ALL</option>
-<?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHERE `sold` = '0' ORDER BY resseller ASC");
-	while($row = mysqli_fetch_assoc($query)){
-		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-	}
-?>
-
- <?php
-include("cr.php");?>
-</select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
-</div>
-
-<div class="container">
-			<table id="example" class="display" width="100%">
-
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Position</th>
-						<th>Office</th>
-						<th>Age</th>
-						<th>Start date</th>
-						<th>Salary</th>
-            <th>buy</th>
-					</tr>
-				</thead>
-			
-<?php
-$q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
- while($row = mysqli_fetch_assoc($q)){
-	 
-	 	 $countryfullname = $row['country'];
-	  $code = array_search("$countryfullname", $countrycodes);
-	 $countrycode = strtolower($code);
-	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
- echo "<tr>     
- <td class='flag-icon flag-icon-$countrycode'> </td>
-	<td>&nbsp;".$row['country']." </td>
-<td>".$row['sitename']."</td>
-<td>".$row['infos']." </td>
-<td>".('$SellerNick')."</td>
-<td> ".$row['price']."</td>
-<td> ".$row['date']."</td>
- echo '<td>
-	<span id="premium'.$row['id'].'" title="buy" type="premium"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
-    </td>
-            </tr>
-     ';
- }
-
- ?>
-	<tfoot>
-	<tr>
-	
-						<th>Name</th>
-						<th>Position</th>
-						<th>Office</th>
-						<th>Age</th>
-						<th>Start date</th>
-						<th>price</th>
-          <th>buy</th>
-					</tr>
-				</tfoot>
-			</table>
-<script type="text/javascript">
-$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
-function buythistool(id){
-  bootbox.confirm("Are you sure?", function(result) {
-        if(result ==true){
-      $.ajax({
-     method:"GET",
-     url:"buytool.php?id="+id+"&t=accounts",
-     dataType:"text",
-     success:function(data){
-         if(data.match(/<button/)){
-		 $("#account"+id).html(data).show();
-         }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-         }
-     },
-   });
-       ;}
-  });
-}
-
-function openitem(order){
-  $("#myModalLabel").text('Order #'+order);
-  $('#myModal').modal('show');
-  $.ajax({
-    type:       'GET',
-    url:        'showOrder'+order+'.html',
-    success:    function(data)
-    {
-        $("#modelbody").html(data).show();
-    }});
-
-}
-
-</script>
-		</div>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body" id="modelbody">
-
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+echo '
+<center>
+<form action="" method="POST">
+username : <input type="text" class="form-control" name="user" style="width : 280px;" /><br> 
+<input type="submit" class="btn btn-primary" value="Search" />
+<input type="hidden" name="start" value="s" />
+</form>
+</center>
+';
+if(isset($_POST['start']) and $_POST['start'] == "s"){
+  $user = mysqli_real_escape_string($dbcon, $_POST['user']);
+  $q = mysqli_query($dbcon, "SELECT * FROM users WHERE username='$user'");
+  echo '<br>
+    <div class="">
+        <div class="panel panel-default">
+            <div class="panel-heading no-collapse">Search Results<span class="label label-warning">'.$t.'</span></div>
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>Balance</th>
+                  <th>items purch</th>
+                  <th>Last login</th>
+                  <th>Edit</th>
+                  <th>Resseller</th>
+                </tr>
+              </thead>
+              <tbody>';
+  while($row = mysqli_fetch_assoc($q)){
+              echo '<tr>
+                  <td>'.$row['username'].'</td>
+                  <td>'.$row['email'].'</td>
+                  <td>'.$row['balance'].'</td>
+                  <td>'.$row['ipurchassed'].'</td>
+                  <td>'.$row['lastlogin'].'</td>
+                  <td>
+				  
+          <a href="user.php?id='.$row['id'].'"><center><span class="btn label-danger"><font color=white>Edit/Delete</font></center></span></a>
+      </td>
+	  <td>';
+      if($row['resseller'] == "0" or empty($row['resseller'])){
+          echo '<a href="activer.php?id='.$row['id'].'" class="btn label-primary"><font color=white>Make resseller</font></a>';
+      }else{
+          echo '<a href="remover.php?id='.$row['id'].'" class="btn label-warning"><font color=white>Ban</font></a>';
+      }
+                  echo '</td>
+				  
+				  
+</tr>';
+  }
+                  echo '

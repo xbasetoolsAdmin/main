650056f7b7e3e1d644e9e215f65dfe8d3bd56ddd
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-05 15:46:04 +0100

    Update premium.php

--- premium.php
+++ premium.php
@@ -5,35 +5,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FIMK</title>
+   <!-- jQuery library -->
+<script src="js/jquery.min.js"></script>
+
+<!-- DataTables CSS and JS library -->
+<link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+<script type="text/javascript" src="DataTables/datatables.min.js"></script>
 </head>
 
 <body>
-    <?php 
-      $a=3; $b=4; $c=5; $d=6;
-      echo Vlera Fillestare e a=.$sa.<br>;
-    ?>
+<div class="post-search-panel">
+    <input type="text" id="searchInput" placeholder="Type keywords..." />
+    <select id="sortBy">
+        <option value="">Sort by</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+    </select>
+</div>
+<table id="memListTable" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Country</th>
+            <th>Created</th>
+            <th>Status</th>
+        </tr>
+    </thead>
    
-l
+    <tfoot>
+        <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Country</th>
+            <th>Created</th>
+            <th>Status</th>
+        </tr>
+    </tfoot>
+</table>
 <script>
-document.addEventListener('DOMContentLoaded',
- function () {
-    let table = new DataTable('#example', {
-        ajax: 
-        function (d, cb) {
-            fetch('../ajax/data/objects.php')
-                .then(response => response.json())
-                .then(data => cb(data));
-        },
-        columns: [
-            { data: 'name' },
-            { data: 'position' },
-            { data: 'office' },
-            { data: 'extn' },
-            { data: 'start_date' },
-            { data: 'salary' }
-        ]
-    } );
-} );
-</script>      
+
+// Initialize DataTables API object and configure table
+var table = $('#memListTable').DataTable({
+"searching": false,
+"processing": true,
+"serverSide": true,
+"ajax": {
+"url": "fetchData.php",
+"data": function ( d ) {
+         return $.extend( {}, d, {
+           "search_keywords": $("#searchInput").val().toLowerCase(),
+           "filter_option": $("#sortBy").val().toLowerCase()
+         } );
+       }
+     }
+});
+
+$(document).ready(function(){
+    // Redraw the table
+    table.draw();
+    
+    // Redraw the table based on the custom input
+    $('#searchInput,#sortBy').bind("keyup change", function(){
+        table.draw();
+    });
+});
+</script>
 </body>
 </html>

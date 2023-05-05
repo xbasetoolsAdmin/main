bc4970323f8db9e9eea11873f4230765a4a64ee8
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-05 20:01:08 +0100

    Update premium.php

--- premium.php
+++ premium.php
@@ -1,86 +1,49 @@
+   <?php
+ob_start();
+session_start();
 
-<!DOCTYPE html>
-<html lang="en">
-   <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FIMK</title>
-   <!-- jQuery library -->
-<script src="files/js/jquery.js"></script>
+date_default_timezone_set('UTC');
+include "../includes/config.php";
 
-<!-- DataTables CSS and JS library -->
-<link rel="stylesheet" type="text/css" href="css/addon/datatables.select.css"/>
+if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){
+   header("location: ../");
+   exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+?>   
+<table width="100%" id="dataTable" class="table table-striped table-bordered table-condensed sticky-header dataTable no-footer" role="grid" aria-describedby="dataTable_info" style="width: 100%;">        <thead>
+  <thead>
+  <tr>
+  <th>ID</th>
+  <th>Type</th>
+  <th>Country</th>
+  <th>Site Name</th>
+  <th>Information</th>
+  <th>Open</th>
+  <th>Date added</th>
+  <th>Price</th>
+  <th>Action</th>
+  </tr>
+        </thead>
+		 <tbody id='tbody2'>
+ <?php
+$uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+$qu = mysqli_query($dbcon, "SELECT * FROM accounts WHERE acctype='account' AND resseller='$uid' ORDER BY id DESC")or die(mysqli_error());
 
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
-</head>
-<body>
-<div class="post-search-panel">
-    <input type="text" id="searchInput" placeholder="Type keywords..." />
-    <select id="sortBy">
-        <option value="">Sort by</option>
-        <option value="Male">Male</option>
-        <option value="Female">Female</option>
-    </select>
-</div>
-<table id="memListTable" class="display" style="width:100%">
-    <thead>
-        <tr>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>Gender</th>
-            <th>Country</th>
-            <th>Created</th>
-            <th>Status</th>
-        </tr>
-    </thead>
+ while($row = mysqli_fetch_assoc($qu)){
+	 
+    echo "<tr class='accounts-tabel'>
+    <td> ".htmlspecialchars($row['id'])." </td>
+    <td> ".strtoupper(htmlspecialchars($row['acctype']))." </td>
+    <td> ".htmlspecialchars($row['country'])." </td>
+    <td> ".htmlspecialchars($row['sitename'])." </td>
+    <td> ".htmlspecialchars($row['infos'])." </td>
+	<td>  "; ?>
    
-    <tfoot>
-        <tr>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>Gender</th>
-            <th>Country</th>
-            <th>Created</th>
-            <th>Status</th>
-        </tr>
-    </tfoot>
-</table>
-<script>
+   
+	<a data-toggle="modal" class="btn btn-primary btn-xs" data-target="#myModald<?php echo  $row['id']; ?>" >
+<font color=white>Open #<? echo htmlspecialchars($row['id']); ?> </a></center> 
 
-// Initialize DataTables API object and configure table
-var table = $('#memListTable').DataTable({
-"searching": false,
-"processing": true,
-"serverSide": true,
-"ajax": {
-"url": "fetchData.php",
-"data": function ( d ) {
-         return $.extend( {}, d, {
-           "search_keywords": $("#searchInput").val().toLowerCase(),
-           "filter_option": $("#sortBy").val().toLowerCase()
-         } );
-       }
-     }
-});
 
-$(document).ready(function(){
-    // Redraw the table
-    table.draw();
-    
-    // Redraw the table based on the custom input
-    $('#searchInput,#sortBy').bind("keyup change", function(){
-        table.draw();
-    });
-});
-</script>
-</body>
-</html>
+ </tbody>
+ </table>

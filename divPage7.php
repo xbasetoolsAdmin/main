c8a5f54b4202cdca9c34e9dd5dbe87373c3a14ae
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-06 00:04:45 +0100

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -1,102 +1,47 @@
 <?php
 ob_start();
 session_start();
-error_reporting();
 date_default_timezone_set('UTC');
+include "includes/config.php";
+include("cr.php");
 
 if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){
    header("location: ../");
    exit();
 }
-  include "includes/config.php";
-?>
-<?php
-
-  $date = date("Y-m-d H:i:s");
-  $uid = mysqli_real_escape_string($dbcon, $_GET['id']);
-  $tbl = mysqli_real_escape_string($dbcon, $_GET['t']);
-
-  $qqs = @mysqli_query($dbcon, "SELECT * FROM $tbl WHERE id='$uid'") or die();
-  $rows = mysqli_fetch_assoc($qqs);
-
-  $price = mysqli_real_escape_string($dbcon, $rows['price']);
-  $type  = mysqli_real_escape_string($dbcon, $rows['acctype']);
-  $fb  = mysqli_real_escape_string($dbcon, $rows['country']);
-  $infos  = mysqli_real_escape_string($dbcon, $rows['infos']);
-  $url  = mysqli_real_escape_string($dbcon, $rows['url']);
-  $login  = mysqli_real_escape_string($dbcon, $rows['login']); //
-  $pa  = mysqli_real_escape_string($dbcon, $rows['pass']); // 
-  $sid  = mysqli_real_escape_string($dbcon, $rows['id']);
-  $resseller  = mysqli_real_escape_string($dbcon, $rows['resseller']); // qi
-
-  $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-  $qqs2 = @mysqli_query($dbcon, "SELECT * FROM users WHERE username='$usrid'") or die();
-  $rows2 = mysqli_fetch_assoc($qqs2);
-
-  $balance =  $rows2['balance'];
-  $ipur =   $rows2['ipurchassed'];
-
-  if($balance >= $price){
-    $newb = $balance - $price;
-    $newb2 = mysqli_real_escape_string($dbcon, $newb);
-    $re = mysqli_query($dbcon, "SELECT sold FROM $tbl WHERE id='$uid'");
-    $ree = mysqli_fetch_assoc($re);
-    if($ree['sold'] == '0'){
-           $npur = $ipur + 1 ;
-			mysqli_query($dbcon, "UPDATE $tbl SET sold='1', sto='$usrid', dateofsold='$date', resseller='$resseller' WHERE id='$uid'");
-			mysqli_query($dbcon, "UPDATE users SET balance='$newb2' WHERE username='$usrid'");
-			mysqli_query($dbcon, "UPDATE users SET ipurchassed='$npur' WHERE username='$usrid'");
-			mysqli_query($dbcon, "INSERT INTO purchases
-            (s_id,buyer,type,date,country,infos,url,login,pass,price,resseller,reported,reportid)
-            VALUES
-            ('$sid','$usrid','$type','$date','$fb','$infos','$url','$login','$pa','$price','$resseller','',null)
-            ");
-      
-            $last_id = mysqli_insert_id($dbcon);
-         $b = $price;
-         mysqli_query($dbcon, "UPDATE resseller SET allsales=(allsales + $b),soldb=(soldb + $b) WHERE username='$resseller'");
-
-	    echo '<button onclick="openitem('.$last_id.')" class="btn btn-primary btn-xs"> Order #'.$last_id.'</button>';
+	function srl($item)
+		{
+		$item0 = $item;
+		$item1 = rtrim($item0);
+		$item2 = ltrim($item1);
 		
-    }else{
-      echo 'Already sold / Deleted.' ;
-    }
-  }else{
-      echo 'Please top-up your balance to buy.' ;
-       }
-//(s_id,buyer,date,type,country,infos,url,login,pass,price,resseller,reported,reportid)
+return $item2;
+		} 
+$usrid     = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+$orderid     = mysqli_real_escape_string($dbcon, $_GET['id']);
+$q = mysqli_query($dbcon, "SELECT * FROM purchases WHERE buyer='$usrid' and id='$orderid'") or die(mysql_error());
+
+while ($row = mysqli_fetch_assoc($q)) {
+	///////////////// Cpanel
+ if ($row['type'] == "cpanel") {
+	 $itemid = $row['s_id'];
+$qe = mysqli_query($dbcon, "SELECT * FROM accounts WHERE id='$itemid'") or die(mysql_error());
 ?>
-<?php
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-$q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE acctype='account' AND resseller='$uid' ORDER BY id DESC")or die(mysqli_error());
 
+ 
+ <thCountry</th>
+  <th>Detect Hosting</th> 
+   <th>Domain</th>
+  <td>.non-https Url</th>
+      <th>Url</th>
 
-<table width="100%" id="dataTable" class="table table-striped table-bordered table-condensed sticky-header dataTable no-footer" role="grid" aria-describedby="dataTable_info" style="width: 100%;">        <thead>
-  <thead>
-  <tr>
-  <th>ID</th>
-  <th>Type</th>
-  <th>Country</th>
-  <th>Site Name</th>
-  <th>Information</th>
-  <th>Open</th>
-  <th>Date added</th>
-  <th>Price</th>
-  <th>Action</th>
-  </tr>
-        </thead>
-      <ttbody2>';>
-  while($row = mysqli_fetch_assoc($q)){
-	 
-    echo "<tr class='accounts-tabel'>
-    <td> ".htmlspecialchars($row['id'])." </td>
-    <td> ".strtoupper(htmlspecialchars($row['acctype']))." </td>
-    <td> ".htmlspecialchars($row['country'])." </td>
-    <td> ".htmlspecialchars($row['sitename'])." </td>
-    <td> ".htmlspecialchars($row['infos'])." </td>
-	 <td> ".htmlspecialchars($row['date'])." </td>
-    <td> ".htmlspecialchars($row['price'])."</td> ;?>
-  
-
- </tbody>
- </table>
+<table class="table"> 
+<td><span class="flag-icon flag-icon-<?php echo htmlspecialchars($countrycode); ?>"> </span> <?php echo htmlspecialchars($country);?> <td>     
+<?php echo htmlspecialchars($hosting);?></td>           
+  <td> <?php echo $domain;?> </td>	
+  <td> <?php echo $maindom;?><td>
+  <td><?php echo $maindom; ?></td>
+  <td><?php echo $maindom;?></td>
+ <td><?php echo $maindom;?></td>
+</tr>	
+</table>

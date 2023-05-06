b73438131ace80abddf8de27380073b29ee16750
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-06 23:59:38 +0100

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -1,92 +1,180 @@
-<?php
+
+<!--?php
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
 include "includes/config.php";
+
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
     header("location: ../");
     exit();
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-?>
-<?php
+?--><html>
+	<head></head>
+	<body>
+		<ul class="nav nav-tabs">
+			<li class="active">
+				<a href="#filter" data-toggle="tab">Filter</a>
+			</li>
+		</ul>
+		<div id="myTabContent" class="tab-content">
+			<div class="tab-pane active in" id="filter">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Country</th>
+							<th>Site Name</th>
+							<th>Seller</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<select class="filterselect form-control input-sm" name="account_country">
+									<option value="">ALL</option>
+									
+									<?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `accounts` WHERE `sold` = '0' ORDER BY country ASC");
+	while($row = mysqli_fetch_assoc($query)){
+	echo '<option value="'.$row['country'].'"'.$row['country'].''; 	} ?&gt;
+								</select>
+							</td>
+							<td>
+								<input class="filterinput form-control input-sm" name="account_sitename" size="3">
+							</td>
+							<td>
+								<select class="filterselect form-control input-sm" name="account_seller">
+									<option value="">ALL</option>
+									
+									<?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHERE `sold` = '0' ORDER BY resseller ASC");
+	while($row = mysqli_fetch_assoc($query)){
+		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = "seller".$rpw["id"]."";
+	echo '<option value="'.$SellerNick.'"'.$SellerNick.''; 	} ?&gt;
+								</select>
+							</td>
+							<td>
+								<button id="filterbutton" class="btn btn-primary btn-sm" disabled="">Filter
+									<span class="glyphicon glyphicon-filter"></span>
+								</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		";     echo '           ';  }   ?&gt;
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">×</span>
+						</button>
+						<h4 class="modal-title" id="myModalLabel"></h4>
+					</div>
+					<div class="modal-body" id="modelbody">
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<table width="100%" class="table table-striped table-bordered table-condensed sticky-header" id="table">
+			<thead>
+				<tr>
+					<th scope="col">Country</th>
+					<th scope="col">Site Name</th>
+					<th scope="col">Available Information</th>
+					<th scope="col">Seller</th>
+					<th scope="col">Price</th>
+					<th scope="col">Added on
+					</th>
+					<th scope="col">Buy</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
 include("cr.php");
-	    
-$q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysql_error());
-
-function srl($item)
-		{
-		$item0 = $item;
-		$item1 = rtrim($item0);
-		$item2 = ltrim($item1);
-		return $item2;
-		} 
-
+$q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
  while($row = mysqli_fetch_assoc($q)){
+	 
 	 	 $countryfullname = $row['country'];
 	  $code = array_search("$countryfullname", $countrycodes);
 	 $countrycode = strtolower($code);
-
-	 $url = $row['url'];
-	 	$d = explode("|", $url);
-		$urled = srl($d[0]);
-
-	 	  $tld = end(explode(".", parse_url($urled, PHP_URL_HOST))); 
-    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
+	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
 		   while($rpw = mysqli_fetch_assoc($qer))
 			 $SellerNick = "seller".$rpw["id"]."";
-     echo " <tr>
-			</div>	"; 	 echo '
-			
-			
-			'; echo "          ";      echo '           
-			
-			';  }   ?&gt;
-			<table id="cpanel_data" class="display dataTable" style="width:100%" aria-describedby="cpanel_data_info">
-				<thead>
-					<tr>
-						<th class="sorting sorting_asc" tabindex="0" aria-controls="cpanel_data" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending" style="width: 51.6875px;">Name</th>
-						<th class="sorting" tabindex="0" aria-controls="cpanel_data" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 68.015625px;">Position</th>
-						<th class="sorting" tabindex="0" aria-controls="cpanel_data" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 47.9375px;">Office</th>
-						<th class="sorting" tabindex="0" aria-controls="cpanel_data" rowspan="1" colspan="1" aria-label="Age: activate to sort column ascending" style="width: 26.1875px;">Age</th>
-						<th class="sorting" tabindex="0" aria-controls="cpanel_data" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 32.4375px;">Start date</th>
-						<th class="sorting" tabindex="0" aria-controls="cpanel_data" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 54.0625px;">Salary</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td id="cpanel_country">
-							<i class="flag-icon flag-icon-$countrycode"></i>&nbsp;".htmlspecialchars($row['country'])."
-						</td>
-						<td id="cpanel_tld">
-							.".$tld."
-						</td>
-						<td id="cpanel_hosting">
-							".htmlspecialchars($row['infos'])."
-						</td>
-						<td id="cpanel_seller">
-							".htmlspecialchars($SellerNick)."</td>
-						<td>
-							<span id="shop'.$row[" id"].'"="" type="cpanel">
-								<a onclick="javascript:check('.$row[" id"].');"="" class="btn btn-info btn-xs">
-									<font color="white">Check</font>
-								</a>
-							</span>
-							<center></center>
-						</td>
-						<td>
-							".htmlspecialchars($row['price'])."</td>
-						<td>
-							".htmlspecialchars($row['date'])."</td>
-						<td>
-							<span id="cpanel'.$row['id'].'" title="buy" type="cpanel">
-								<a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs">
-									<font color="white">Buy</font>
-								</a>
-							</span>
-							<center>
-							</center>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+     echo "
+ <tr>
+				<tr>
+					<td id="account_country">
+						<i class="flag-icon flag-icon-$countrycode"></i>&nbsp;".htmlspecialchars($row['country'])."
+					</td>
+					<td id="account_sitename">
+						".htmlspecialchars($row['sitename'])."
+					</td>
+					<td>
+						".htmlspecialchars($row['infos'])."
+					</td>
+					<td id="account_seller">
+						".htmlspecialchars($SellerNick)."</td>
+					<td>
+						".htmlspecialchars($row['price'])."</td>
+					<td>
+						".$row['date']."</td>
+					<td>
+						<span id="premium'.$row['id'].'" title="buy" type="premium">
+							<a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs">
+								<font color="white">Buy</font>
+							</a>
+						</span>
+						<center>
+						</center>
+					</td>
+				</tr>
+				<script type="text/javascript">
+$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
+function buythistool(id){
+  bootbox.confirm("Are you sure?", function(result) {
+        if(result ==true){
+      $.ajax({
+     method:"GET",
+     url:"buytool.php?id="+id+"&t=accounts",
+     dataType:"text",
+     success:function(data){
+         if(data.match(/<button/)){
+		 $("#account"+id).html(data).show();
+         }else{
+            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
+         }
+     },
+   });
+       ;}
+  });
+}
+
+function openitem(order){
+  $("#myModalLabel").text('Order #'+order);
+  $('#myModal').modal('show');
+  $.ajax({
+    type:       'GET',
+    url:        'showOrder'+order+'.html',
+    success:    function(data)
+    {
+        $("#modelbody").html(data).show();
+    }});
+
+}
+
+
+				</script>
+			</tbody>
+		</table>
+	</body>
+</html>

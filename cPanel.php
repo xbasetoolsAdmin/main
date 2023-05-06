e0a7dfe0195879f08c631044d5c21341792e63f2
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-06 18:52:58 +0100

    Update cPanel.php

--- cPanel.php
+++ cPanel.php
@@ -1,5 +1,4 @@
-
-<!--?php
+<?php
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
@@ -9,13 +8,14 @@ ob_start();
 session_start();
 date_default_timezone_set('UTC');
 include "includes/config.php";
-
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
     header("location: ../");
     exit();
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-?--><!DOCTYPE html>
+?>
+
+<!DOCTYPE html>
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1">

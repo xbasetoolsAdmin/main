b3d41cb8dee2e81087e8560c7316c373f6c341a9
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-06 22:47:16 +0100

    Update premium.php

--- premium.php
+++ premium.php
@@ -4,11 +4,6 @@ ob_start();
 session_start();
 date_default_timezone_set('UTC');
 include "includes/config.php";
-<?php
-ob_start();
-session_start();
-date_default_timezone_set('UTC');
-include "includes/config.php";
 
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
     header("location: ../");

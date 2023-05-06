05b518466c085eb72ed8f86822677127bb63cf65
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-07 00:35:29 +0100

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -1,5 +1,5 @@
 
-<!--?php
+<!?php
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
@@ -10,7 +10,8 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
     exit();
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-?--><html>
+?>
+<html>
 	<head></head>
 	<body>
 		<ul class="nav nav-tabs">
@@ -33,22 +34,21 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 						<tr>
 							<td>
 								<select class="filterselect form-control input-sm" name="account_country">
-									<option value="">ALL</option>
+									<option value="All">ALL</option>
 									
-									<?php
+<?php
 $query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `accounts` WHERE `sold` = '0' ORDER BY country ASC");
 	while($row = mysqli_fetch_assoc($query)){
-	echo '<option value="'.$row['country'].'"'.$row['country'].''; 	} ?&gt;
-								</select>
+	echo '<option value="All'.$row['country'].'"'.$row['country'].'country'; 	} ?&gt;
+</select>
 							</td>
 							<td>
-								<input class="filterinput form-control input-sm" name="account_sitename" size="3">
-							</td>
-							<td>
-								<select class="filterselect form-control input-sm" name="account_seller">
-									<option value="">ALL</option>
-									
-									<?php
+<input class="filterinput form-control input-sm" name="account_sitename" size="3">
+	</td>
+<td>
+<select class="filterselect form-control input-sm" name="account_seller">
+<option value="All">ALL</option>
+<?php
 $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHERE `sold` = '0' ORDER BY resseller ASC");
 	while($row = mysqli_fetch_assoc($query)){
 		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());

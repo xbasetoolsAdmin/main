faeb39b7e672a29b178c8fc39a63842cbec5384b
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-07 16:58:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 
 <!doctype html>
 <html>
-<head>
+<head><meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> <meta name="referrer" content="no-referrer" />
 <link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
 <link rel="stylesheet" type="text/css" href="files/css/flags.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
@@ -25,10 +25,9 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <script type="text/javascript" src="files/js/clipboard.min.js"></script>
 
 <link rel="shortcut icon" href="files/img/favicon.ico" />
-<meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> 
- <meta name="referrer" content="no-referrer" />
-<script type="text/javascript" src="files/js/jquery.js?1"></script>
-<script type="text/javascript" src="common.js"></script>
+
+<script type="text/javascript" src="code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="utf-8">
 <title>JeruxShop</title>

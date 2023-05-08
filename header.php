8af24b4b3f636ed83ec7597c8d677ff927325adc
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-08 02:01:48 +0100

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,239 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
+	  
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Main - Admin Panel</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/all.min.css">
+	<link href="assets/css/style.min.css" rel="stylesheet">	
+	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="files/css/flags.css">
+	<link rel="stylesheet" href="assets/fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
+	<style>body{padding-top:80px}</style>
+	<font face="Arial">
+<script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/bootbox.min.js"></script>
+<script type="text/javascript" src="assets/js/bootbox.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
+<script src="assets/main.js"></script>
+<script type="text/javascript">
+            // Notice how this gets configured before we load Font Awesome
+            window.FontAwesomeConfig = { autoReplaceSvg: false }
+</script>
+<style>
+            @import url(//fonts.googleapis.com/css?family=Roboto:400);
+            .navbar-nav .dropdown-menu
+            {
+            margin:0 !important
+            }
+			</style>		
+            </head>
+<style>
 
+</style>
+
+<body class="them">
+ <style>
+        .navbar-nav .dropdown-menu {
+            margin: 0 !important
+        }
+ 
+        .theme-light {
+            --color-primary: #0060df;
+            --color-secondary: #ffffff;
+            --color-secondary2: #ecf0f1;
+            --color-accent: #fd6f53;
+            --font-color: #000000;
+            --color-nav: #ffffff;
+            --color-dropdown: #ffffff;
+            --color-card: #ffffff;
+            --color-card2: #d1ecf1;
+            --color-info: #0c5460;
+            --color-backinfo: #d1ecf1;
+            --color-borderinfo: #bee5eb;
+ 
+        }
+ 
+        .theme-dark {
+            --color-primary: #17ed90;
+            --color-secondary: #353B50;
+            --color-secondary2: #353B50;
+            --color-accent: #12cdea;
+            --font-color: #ffffff;
+            --color-nav: #363947;
+            --color-dropdown: rgba(171, 205, 239, 0.3);
+            --color-card: #262A37;
+            --color-card2: #262A37;
+            --color-info: #4DD0E1;
+            --color-backinfo: #262A37;
+            --color-borderinfo: #262A37;
+        }
+ 
+        .them {
+ 
+            background: var(--color-secondary);
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+ 
+        .them h1 {
+            color: var(--font-color);
+            font-family: sans-serif;
+        }
+ 
+        .card-body {
+            color: var(--font-color);
+        }
+ 
+        .them button {
+            color: var(--font-color);
+            background-color: #ffffff;
+            padding: 10px 20px;
+            border: 0;
+            border-radius: 5px;
+        }
+ 
+        .navbar.navbar-light .navbar-toggler {
+            color: var(--font-color);
+        }
+ 
+        /* The switch - the box around the slider */
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 60px;
+            height: 34px;
+        }
+ 
+        /* Hide default HTML checkbox */
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        } 
+ 
+        /* The slider */
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            -webkit-transition: 0.4s;
+            transition: 0.4s;
+        }
+ 
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 40px;
+            width: 40px;
+            left: 0px;
+            bottom: 4px;
+            top: 0;
+            bottom: 0;
+            margin: auto 0;
+            -webkit-transition: 0.4s;
+            transition: 0.4s;
+            box-shadow: 0 0px 15px #2020203d;
+            background: white url('https://i.ibb.co/FxzBYR9/night.png');
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+ 
+        input:checked+.slider {
+            background-color: #2196f3;
+        }
+ 
+        input:focus+.slider {
+            box-shadow: 0 0 1px #2196f3;
+        }
+ 
+        input:checked+.slider:before {
+            -webkit-transform: translateX(24px);
+            -ms-transform: translateX(24px);
+            transform: translateX(24px);
+            background: white url('https://i.ibb.co/7JfqXxB/sunny.png');
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+ 
+        /* Rounded sliders */
+        .slider.round {
+            border-radius: 34px;
+        }
+ 
+        .slider.round:before {
+            border-radius: 50%;
+        }
+	
+</style>
+
+<style>
+    .display td {
+        background: var(--color-card);
+        color: var(--font-color);
+ 
+    }
+ 
+    .dataTables_wrapper .dataTables_paginate .paginate_button {
+ 
+        color: var(--font-color);
+ 
+    }
+ 
+    .dataTable_paginate .paginate_button {
+        color: var(--font-color);
+ 
+    }
+ 
+    .alert-info {
+        color: var(--color-info);
+        background-color: var(--color-backinfo);
+        border-color: var(--color-borderinfo);
+ 
+    }
+ 
+    .dataTable_filter {
+        color: var(--font-color);
+		
+    }
+ 
+    .dataTable_length {
+        color: var(--font-color);
+    }
+ 
+    .dataTable_paginate {
+        color: var(--font-color);
+    }
+ 
+    .dataTable_info {
+        color: var(--font-color);
+    }
+</style>
+
+ <style>
+ 
+ 
+</style>
 
 
 		<style type="text/css">

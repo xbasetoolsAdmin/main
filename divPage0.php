bb60e42a1906bb910562f118e5c5c4a990a7d2e8
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-08 14:17:14 +0100

    Update divPage0.php

--- divPage0.php
+++ divPage0.php
@@ -279,21 +279,6 @@ if ($r1 == "1") {
   </div>
   <!-- /.container-fluid -->
 </nav>
-<?php
-ob_start();
-session_start();
-error_reporting();
-date_default_timezone_set('UTC');
-include "includes/config.php";
-
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-    header("location: ../");
-    exit();
-}
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-
-?>
-
 
 <?php
  echo'

0ee2f32c98a09cb8ff5f83a130b19b4500a802b9
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-09 00:00:07 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -1,13 +1,5 @@
-<?php
-ob_start();
-session_start();
-date_default_timezone_set('UTC');
-include "includes/config.php";
-
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-    header("location: ../");
-    exit();
-}
+<?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {   header("location: ../");
+    exit();}
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
 <!doctype html>
@@ -206,18 +198,33 @@ if ($r1 == "1") {
     </div>
   </div>
 </div>
- <script type="text/javascript">
+</div>   
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+      <div class="modal-body" id="modelbody">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
                     $('#filterbutton').click(function() {
                         $("#table tbody tr").each(function() {
-				
-                            var ck1 = $.trim($(this).find("#scam_name").text().toLowerCase());
-                            var ck2 = $.trim($(this).find("#scam_info").text().toLowerCase());
-                            var ck3 = $.trim($(this).find("#scam_seller").text().toLowerCase());
-				
-                            var val1 = $.trim($('input[name="scam_name"]').val().toLowerCase());
-                            var val2 = $.trim($('input[name="scam_info"]').val().toLowerCase());
-                            var val3 = $.trim($('select[name="scam_seller"]').val().toLowerCase());
-                            if ((ck1 != val1 && val1 != '') || ck2.indexOf(val2) == -1 || (ck3 != val3 && val3 != '')) {
+                            var ck1 = $.trim($(this).find("#tuto_name").text().toLowerCase());
+                            var ck2 = $.trim($(this).find("#tuto_info").text().toLowerCase());
+                            var ck3 = $.trim($(this).find("#tuto_seller").text().toLowerCase());
+                            var val1 = $.trim($('input[name="tuto_name"]').val().toLowerCase());
+                            var val2 = $.trim($('input[name="tuto_info"]').val().toLowerCase());
+                            var val3 = $.trim($('select[name="tuto_seller"]').val().toLowerCase());
+                         
+	if ((ck1 != val1 && val1 != '') || ck2.indexOf(val2) == -1 || (ck3 != val3 && val3 != '')) {
                                 $(this).hide();
                             } else {
                                 $(this).show();
@@ -232,18 +239,16 @@ if ($r1 == "1") {
                         $('#filterbutton').prop('disabled', false);
                     });
 
-          function buythistool(id){
-           bootbox.confirm("Are you sure?", 
-			   
-     function(result) {
+function buythistool(id){
+  bootbox.confirm("Are you sure?", function(result) {
         if(result ==true){
-         $.ajax({
-          method:"GET",
-           url:"buyscam.php?id="+id+"&t=scampages",
-           dataType:"text",
-            success:function(data){
+      $.ajax({
+     method:"GET",
+     url:"buytuto.php?id="+id+"&t=tutorials",
+     dataType:"text",
+     success:function(data){
          if(data.match(/<button/)){
-		 $("#scam"+id).html(data).show();
+		 $("#tuto"+id).html(data).show();
          }else{
             bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
          }
@@ -252,7 +257,7 @@ if ($r1 == "1") {
        ;}
   });
 }
-	 
+
 function openitem(order){
   $("#myModalLabel").text('Order #'+order);
   $('#myModal').modal('show');
@@ -267,7 +272,4 @@ function openitem(order){
 }
 
 </script>
-
-</body>
-</html>
 

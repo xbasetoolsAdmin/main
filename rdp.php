c66094e4030e409d653c1f997d8a6a75f6be8769
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-09 00:41:13 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -180,7 +180,21 @@ if ($r1 == "1") {
   <!-- /.container-fluid -->
 </nav>
 <?php include"divpage/divpage1.php";?>
-
+<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
+<thead>
+    <tr>
+      <th scope="col" >Country</th>
+      <th scope="col">TLD</th>
+      <th scope="col">Detect Hosting</th>
+      <th scope="col">Seller</th>
+      <th scope="col">Check</th>
+      <th scope="col">Price</th>
+      <th scope="col">Added on </th>
+      <th scope="col">Buy</th>
+    </tr>
+</thead>
+</table>
+</div>
 <div id="mainDiv"></div>
 
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -216,62 +230,4 @@ if ($r1 == "1") {
     </div>
   </div>
 </div>
-<script type="text/javascript">
-                    $('#filterbutton').click(function() {
-                        $("#table tbody tr").each(function() {
-                            var ck1 = $.trim($(this).find("#tuto_name").text().toLowerCase());
-                            var ck2 = $.trim($(this).find("#tuto_info").text().toLowerCase());
-                            var ck3 = $.trim($(this).find("#tuto_seller").text().toLowerCase());
-                            var val1 = $.trim($('input[name="tuto_name"]').val().toLowerCase());
-                            var val2 = $.trim($('input[name="tuto_info"]').val().toLowerCase());
-                            var val3 = $.trim($('select[name="tuto_seller"]').val().toLowerCase());
-                         
-	if ((ck1 != val1 && val1 != '') || ck2.indexOf(val2) == -1 || (ck3 != val3 && val3 != '')) {
-                                $(this).hide();
-                            } else {
-                                $(this).show();
-                            }
-                        });
-                        $('#filterbutton').prop('disabled', true);
-                    });
-                    $('.filterselect').change(function() {
-                        $('#filterbutton').prop('disabled', false);
-                    });
-                    $('.filterinput').keyup(function() {
-                        $('#filterbutton').prop('disabled', false);
-                    });
-
-function buythistool(id){
-  bootbox.confirm("Are you sure?", function(result) {
-        if(result ==true){
-      $.ajax({
-     method:"GET",
-     url:"buytuto.php?id="+id+"&t=tutorials",
-     dataType:"text",
-     success:function(data){
-         if(data.match(/<button/)){
-		 $("#tuto"+id).html(data).show();
-         }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-         }
-     },
-   });
-       ;}
-  });
-}
-
-function openitem(order){
-  $("#myModalLabel").text('Order #'+order);
-  $('#myModal').modal('show');
-  $.ajax({
-    type:       'GET',
-    url:        'showOrder'+order+'.html',
-    success:    function(data)
-    {
-        $("#modelbody").html(data).show();
-    }});
-
-}
-
-</script>
 

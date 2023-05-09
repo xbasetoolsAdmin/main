4c0f74271f50c25771220791b79c613882f5dc18
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-09 01:43:03 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -179,9 +179,10 @@ if ($r1 == "1") {
   </div>
   <!-- /.container-fluid -->
 </nav>
+
 <?php include"divpage/divpage1.php";?>
 <table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
-<thead>
+  <thead>
     <tr>
       <th scope="col" >Country</th>
       <th scope="col">TLD</th>
@@ -193,6 +194,7 @@ if ($r1 == "1") {
       <th scope="col">Buy</th>
     </tr>
 </thead>
+	<div id="mainDiv">
 </table>
 </div>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">

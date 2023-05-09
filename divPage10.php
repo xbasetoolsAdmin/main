015e093ad899954d2da43341d47ab6441d0ca5ae
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-09 02:49:37 +0100

    Update divPage10.php

--- divPage10.php
+++ divPage10.php
@@ -1,97 +1,70 @@
-<?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {  header("location: ../");
-                    exit();}
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
-
-   
-
-
-
-
-
-
-<ul class="nav nav-tabs">
-               <li class="active">
-		   <a href="#filter" data-toggle="tab">Filter</a></li>
-    </ul>
-    <div id="myTabContent" class="tab-content">
-        <div class="tab-pane active in" id="filter">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Tutorial Name</th>
-                        <th>Description</th>
-                        <th>Seller</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-  <td>
-                            <input class='filterinput form-control input-sm' name="tuto_name" size='3'>
-                        </td>
-                        <td>
-                            <input class='filterinput form-control input-sm' name="tuto_info" size='3'>
-                        </td>
-                        <td>
-<select class='filterselect form-control input-sm' name="tuto_seller">
-                                <option value="">ALL</option>
 <?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `tutorials` WHERE `sold` = '0' or `sold` = '1' ORDER BY resseller ASC");
-	while($row = mysqli_fetch_assoc($query)){
-		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-	}
+ob_start();
+session_start();
+date_default_timezone_set('UTC');
+include "includes/config.php";
+
+if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
+    header("location: ../");
+    exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
-                            </select>
-                        </td>
-                        <td>
-                            <button id='filterbutton' class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
 
-    <table width="100%" class="table table-striped table-bordered table-condensed sticky-header" id="table">
-        <thead>
-            <tr>
-                <th scope="col">Tutorial Name</th>
-                <th scope="col">Description</th>
-                <th scope="col">Seller</th>
-                <th scope="col">Price</th>
-                <th scope="col">Added on </th>
-                <th scope="col">Buy</th>
-            </tr>
-        </thead>
-        <tbody>
+<div class="table-responsive">
+  <table class="table table-hover table-sm border-success">
+    <thead>
+      <tr>
+        <th scope="col"> </th>
+    </thead>
+    <tbody>
+<?php
+		include("cr.php");
+	    $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysql_error());
+	   	function srl($item)
+		{
+		$item0 = $item;
+		$item1 = rtrim($item0);
+		$item2 = ltrim($item1);
+		return $item2;
+		} 
 
-            <?php
-$q = mysqli_query($dbcon, "SELECT * FROM tutorials WHERE sold='0' or sold='1' ORDER BY RAND()")or die(mysqli_error());
  while($row = mysqli_fetch_assoc($q)){
-	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
+	 	 $countryfullname = $row['country'];
+	  $code = array_search("$countryfullname", $countrycodes);
+	 $countrycode = strtolower($code);
+
+	 $url = $row['url'];
+	 	$d = explode("|", $url);
+		$urled = srl($d[0]);
+
+	 	  $tld = end(explode(".", parse_url($urled, PHP_URL_HOST))); 
+    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
 		   while($rpw = mysqli_fetch_assoc($qer))
 			 $SellerNick = "seller".$rpw["id"]."";
      echo "
- <tr>     
-    <td id='tuto_name'> ".htmlspecialchars($row['tutoname'])." </td> 
-	<td id='tuto_info'> ".htmlspecialchars($row['infos'])." </td>
-    <td id='tuto_seller'> ".htmlspecialchars($SellerNick)."</td>
-    <td> ".htmlspecialchars($row['price'])."</td>
-	    <td> ".$row['date']."</td>";
+ <tr>   
+    <td".htmlspecialchars($row['id'])." </td>   
+   <td><i class='flag-icon flag-icon-$countrycode'></i></td>
+   <td>&nbsp;".htmlspecialchars($row['country'])." </td>
+      <td'> .".$tld." </td>
+        <td> ".htmlspecialchars($row['sitename'])." </td>
+    <td> ".htmlspecialchars($row['infos'])." </td>
+    <td> ".htmlspecialchars($SellerNick)."</td>";
+	 echo '<td><span id="shop'.$row["id"].'" type="cpanel"><a onclick="javascript:check('.$row["id"].');" class="btn btn-info btn-xs"><font color=white>Check</font></a></span><center></td>';
+	 echo " <td> ".htmlspecialchars($row['price'])."</td>
+      <td> ".htmlspecialchars($row['date'])."</td>
+    ";
+
     echo '
     <td>
-	<span id="tuto'.$row['id'].'" title="buy" type="tuto"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
+	<span id="acounts'.$row['id'].'" title="buy" type="accounts"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
     </td>
             </tr>
      ';
  }
 
  ?>
-           </tr>
-                </tbody>
-            </table>
-        </div>
 
+ </tbody>
+ </table>

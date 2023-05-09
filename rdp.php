820a10222648fb147d5016f3c2ae5deed40da889
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-09 01:02:27 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -195,8 +195,6 @@ if ($r1 == "1") {
 </thead>
 </table>
 </div>
-<div id="mainDiv"></div>
-
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">

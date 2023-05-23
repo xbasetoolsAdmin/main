b979e23e9eb6f0ff7efaa5d3e013dd9d0355f58d
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 00:18:17 +0100

    Update index.php

--- main/index.php
+++ main/index.php
@@ -1,1 +1,3 @@
-
+<?php include('authentication/database.php'); ?><?php include('authentication/auth-controller.php'); ?>
+<?php include('../authentication/security.php'); ?>
+<?php include('includes/header.php'); ?>

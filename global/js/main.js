9c4a7749cdab2266ac6b8cb106323a19608b1cdc
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-23 12:23:38 -0700

    Delete global directory

--- global/js/main.js
+++
@@ -1,1 +0,0 @@
-$(function(){'use strict';$('.navbar-nav li.dropdown').hover(function(){$(this).find('.dropdown-menu').stop(true,true).delay(100).fadeIn(300);},function(){$(this).find('.dropdown-menu').stop(true,true).delay(100).fadeOut(300);});});
--- global/main.js
+++
@@ -1,1 +0,0 @@
-$((function(){$(".navbar-nav li.dropdown").hover((function(){$(this).find(".dropdown-menu").stop(!0,!0).delay(100).fadeIn(300)}),(function(){$(this).find(".dropdown-menu").stop(!0,!0).delay(100).fadeOut(300)}))}));

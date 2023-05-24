f57a04a8e7f36f34634a8b34dab5368c48f3dd29
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 00:37:07 +0100

    Update main.php

--- main/main.php
+++ main/main.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<?php
-   ob_start();
-   session_start();
-   date_default_timezone_set('UTC');
-   include "includes/config.php";
-   include"p_header.php";
-   if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-       header("location: ../");
-       exit();
-   }
-   $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-   ?>
-<!doctype html>
-<script type="application/json">
-pageDiv(0, ' - ', '', 1);
-</script>
 
-
-
-
-
-
-
-<div id="mainDiv" table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
 <script type="text/javascript">
     $(document).keydown(function(event){
         if(event.which=="17")
@@ -44,7 +20,7 @@ pageDiv(0, ' - ', '', 1);
 
     $(window).on('load', function() {
 		$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
-	   	pageDiv(0,'Add Balance - XBASEOOLS','',1);
+	   	pageDiv(2,'header - XBASEOOLS','',1);
 	   var clipboard = new Clipboard('.copyit');
 	    clipboard.on('success', function(e) {
 	      setTooltip(e.trigger, 'Copied!');

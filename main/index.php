57565abdadfc7c338733436db0cd2f632546671c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 03:08:04 +0100

    Update index.php

--- main/index.php
+++ main/index.php
@@ -14,7 +14,7 @@
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
-include "includes/config.php";
+include "authentication/database.php";
 
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
     header("location: ../");

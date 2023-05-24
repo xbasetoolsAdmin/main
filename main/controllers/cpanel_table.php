577b687503937c3626a49b235211f16598502b77
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 19:14:48 +0100

    Update cpanel_table.php

--- main/controllers/cpanel_table.php
+++ main/controllers/cpanel_table.php
@@ -43,7 +43,12 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `cpanels` WHERE
 
 <table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
 <thead>
-    <tr><th scope="col" >Country</th> <th scope="col">TLD</th> <th scope="col">Hosting</th>   <th scope="col">Seller</th><th scope="col">Check</th><th scope="col">Price</th><th scope="col">Added on </th><th scope="col">Buy</th>  </tr>
+    <tr><th scope="col" >Country</th> 
+      <th scope="col">TLD</th>
+       <th scope="col">Hosting</th>  
+       <th scope="col">Seller</th>
+      <th scope="col">Check</th>
+      <th scope="col">Price</th><th scope="col">Added on </th><th scope="col">Buy</th>  </tr>
                         </thead>
                     </table>
                 </div>

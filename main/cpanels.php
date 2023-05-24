e86b20864cd9efb119e014b2d2b37acd9da52284
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 14:44:00 -0700

    Update cpanels.php

--- main/cpanels.php
+++ main/cpanels.php
@@ -22,10 +22,6 @@
 
 
 
-
-<div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-<div class="col-sm-12 Id="mainDiv" table-responsive">
- <table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
                 
 
 

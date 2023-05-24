7f9b69a8327faff00749b5393e3cc376e4c2f0f9
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 06:50:19 -0700

    Create cpanels.php

---
+++ main/cpanels.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<?php
+   ob_start();
+   session_start();
+   date_default_timezone_set('UTC');
+   include "includes/config.php";
+   include"p_header.php";
+   if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
+       header("location: ../");
+       exit();
+   }
+   $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+   ?>
+<!doctype html>
+<html>
+
+
+
+
+
+
+
+
+<div id="mainDiv"></div>
+<script type="text/javascript">
+
+    $(document).keydown(function(event){
+        if(event.which=="17")
+            cntrlIsPressed = true;
+    });
+
+    $(document).keyup(function(){
+        cntrlIsPressed = false;
+    });
+
+    var cntrlIsPressed = false;
+
+
+    $(window).on("popstate", function(e) {
+        location.replace(document.location);
+
+    });
+
+
+    $(window).on('load', function() {
+		$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
+	   	pageDiv(0,'Add Balance - XBASEOOLS','',1);
+	   var clipboard = new Clipboard('.copyit');
+	    clipboard.on('success', function(e) {
+	      setTooltip(e.trigger, 'Copied!');
+	      hideTooltip(e.trigger);
+	      e.clearSelection();
+	   });
+
+});
+
+
+function setTooltip(btn, message) {
+  //console.log("hide-1");
+  $(btn).tooltip('hide')
+    .attr('data-original-title', message)
+    .tooltip('show');
+     //console.log("show");
+}
+
+function hideTooltip(btn) {
+  setTimeout(function() {$(btn).tooltip('hide'); /*console.log("hide-2");*/}, 1000);
+}
+</script>
+</body>
+</html>
+
+
+
+
+
+

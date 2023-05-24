ed2fb4a8a1743ce043e045675b8fa6fab73d1a78
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 22:41:37 +0100

    Update data02.js

--- main/category/data02.js
+++ main/category/data02.js
@@ -1,3 +1,115 @@
+function ajaxinfo() {
+        $.ajax({
+            type: 'GET',
+            url: 'ajaxinfo.html',
+            timeout: 10000,
+ 
+            success: function(data) {
+                if (data != '01') {
+                    var data = JSON.parse(data);
+                    for (var prop in data) {
+                        $("#" + prop).html(data[prop]).show();
+                    }
+                } else {
+                    window.location = "logout.html";
+                }
+            }
+        });
+ 
+    }
+    setInterval(function() {
+        ajaxinfo()
+    }, 3000);
+ 
+    ajaxinfo();
+ 
+    $(document).keydown(function(event) {
+        if (event.which == "17")
+            cntrlIsPressed = true;
+    });
+ 
+    $(document).keyup(function() {
+        cntrlIsPressed = false;
+    });
+ 
+    var cntrlIsPressed = false;
+ 
+ 
+    function pageDiv(n, t, u, x) {
+        if (cntrlIsPressed) {
+            window.open(u, '_blank');
+            return false;
+        }
+        var obj = {
+            Title: t,
+            Url: u
+        };
+        if (("/" + obj.Url) != location.pathname) {
+            if (x != 1) {
+                history.pushState(obj, obj.Title, obj.Url);
+            } else {
+                history.replaceState(obj, obj.Title, obj.Url);
+            }
+ 
+        }
+        document.title = obj.Title;
+        $("#mainDiv").html('<div id="mydiv"><img src="files/img/load2.gif" class="ajax-loader"></div>').show();
+        $.ajax({
+            type: 'GET',
+            url: 'divPage' + n + '.html',
+            success: function(data) {
+                $("#mainDiv").html(data).show();
+                newTableObject = document.getElementById('table');
+               // sorttable.makeSortable(newTableObject);
+             //   $(".sticky-header").floatThead({
+                    top: 60
+                });
+                if (x == 0) {
+                    ajaxinfo();
+                }
+            }
+        });
+        if (typeof stopCheckBTC === 'function') {
+            var a = stopCheckBTC();
+        }
+ 
+    }
+ 
+    $(window).on("popstate", function(e) {
+        location.replace(document.location);
+ 
+    });
+ 
+    $(window).on('load', function() {
+        $('.dropdown').hover(function() {
+            $('.dropdown-toggle', this).trigger('click');
+        });
+        pageDiv(0, 'Main - FeluxShop', 'index.html', 1);
+        var clipboard = new Clipboard('.copyit');
+        clipboard.on('success', function(e) {
+            setTooltip(e.trigger, 'Copied!');
+            hideTooltip(e.trigger);
+            e.clearSelection();
+        });
+ 
+    });
+ 
+ 
+    function setTooltip(btn, message) {
+        console.log("hide-1");
+        $(btn).tooltip('hide')
+            .attr('data-original-title', message)
+            .tooltip('show');
+        console.log("show");
+    }
+ 
+    function hideTooltip(btn) {
+        setTimeout(function() {
+            $(btn).tooltip('hide');
+            console.log("hide-2");
+        }, 1000);
+    }
+
 $('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#cpanel_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#cpanel_tld").text().toLowerCase() );var ck3 = $.trim( $(this).find("#cpanel_hosting").text().toLowerCase() );var ck4 = $.trim( $(this).find("#cpanel_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="cpanel_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="cpanel_tld"]').val().toLowerCase() );var val3 = $.trim( $('input[name="cpanel_hosting"]').val().toLowerCase() );var val4 = $.trim( $('select[name="cpanel_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || ck3.indexOf(val3)==-1 || (ck4 != val4 && val4 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
 
 

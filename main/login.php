4abe2fd07e6209f3c1ef49a4a7a5b02041f4468e
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 09:45:16 +0100

    Update login.php

--- main/login.php
+++ main/login.php
@@ -1,4 +1,4 @@
-<?php  ob_start();  session_start();  include "authentication/database.php";  date_default_timezone_set('UTC'); if(isset($_SESSION['sname']) and isset($_SESSION['spass'])){   header("location: ../login.html");
+<?php  ob_start();  session_start();  include "authentication/database.php";  date_default_timezone_set('UTC'); if(isset($_SESSION['sname']) and isset($_SESSION['spass'])){   header("location: ../login.php");
  exit();
 }
 ?>

5783b4e2aa5bc126b788cf51132553db7d326260
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 14:28:16 -0700

    Update index.php

--- main/index.php
+++ main/index.php
@@ -20,49 +20,8 @@
 
 
 <div id="mainDiv" table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
-<script type="text/javascript">
-    $(document).keydown(function(event){
-        if(event.which=="17")
-            cntrlIsPressed = true;
-    });
-
-    $(document).keyup(function(){
-        cntrlIsPressed = false;
-    });
-
-    var cntrlIsPressed = false;
-
-
-    $(window).on("popstate", function(e) {
-        location.replace(document.location);
-
-    });
-
-
-    $(window).on('load', function() {
-		$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
-	   	pageDiv(0,'Add Balance - XBASEOOLS','',1);
-	   var clipboard = new Clipboard('.copyit');
-	    clipboard.on('success', function(e) {
-	      setTooltip(e.trigger, 'Copied!');
-	      hideTooltip(e.trigger);
-	      e.clearSelection();
-	   });
-
-});
-
-
-function setTooltip(btn, message) {
-  //console.log("hide-1");
-  $(btn).tooltip('hide')
-    .attr('data-original-title', message)
-    .tooltip('show');
-     //console.log("show");
-}
-
-function hideTooltip(btn) {
-  setTimeout(function() {$(btn).tooltip('hide'); /*console.log("hide-2");*/}, 1000);
-}
+ <!doctype html>
+<script type="application/json">
+pageDiv(0, ' - ', '', 1);
 </script>
-</body>
 </html>

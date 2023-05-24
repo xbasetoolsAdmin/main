516adcfce93bde7d73e77ef2dd1c64ca1033d995
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 18:58:53 +0100

    Update cpanel_table.php

--- main/controllers/cpanel_table.php
+++ main/controllers/cpanel_table.php
@@ -56,13 +56,7 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `cpanels` WHERE
     </tr>
 </thead>
   <tbody>
-
-
- </tbody>
  </table>
-<script type="text/javascript">
-  
-</script>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -80,3 +74,38 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `cpanels` WHERE
     </div>
   </div>
 </div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+      <div class="modal-body" id="modelbody">
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
+<script type="text/javascript">
+		$(document).ready(function() {
+			$('#table').DataTable( {
+				"lengthMenu":
+         [
+                   [10, 25, 100, 500, -1], 
+                   [10, 25, 100, 500, "All"]
+         
+        ],
+				'iDisplayLength': 1000,
+				"aaSorting": []
+			} );
+		} );
+		
+</script>

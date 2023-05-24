8525e20ba19a553157d392343e4eb5a7b070bc26
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-24 19:29:33 +0100

    Update cpanels.php

--- main/cpanels.php
+++ main/cpanels.php
@@ -13,7 +13,6 @@
    $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
    ?>
 <!doctype html>
-<html>
 
 
 
@@ -22,8 +21,14 @@
 
 
 
-<div id="mainDiv"></div>
-<script type="text/javascript">
+<div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+
+<div class="col-sm-12 Id="mainDiv" table-responsive">
+    
+ <table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
+                
+
+
 
 
 

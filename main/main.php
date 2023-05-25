ab11a4d8cf1473f068e08bc6cf5d01b98ec5b26e
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 04:42:38 -0700

    Update main.php

--- main/main.php
+++ main/main.php
@@ -1,3 +1,8 @@
 
 
-<div id="mainDiv">
+<div style="padding-left:20px; display:inline-block; margin-bottom:10px">
+<span><a href="addBalance.php?do=balance" class="btn btn-primary" style="color:#fff; font-size: 12 px">Add Balance</a></span>
+<span><a href="addBalance.php?do=details" class="btn btn-primary" style="color:#fff; font-size: 12 px">Balance Info</a></span>
+</div>
+<div id="mainDiv"></div>
+<script type="text/javascript">

f62cd53ab44c1085769aa26d66a9114afa0bef0c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 15:55:13 +0100

    Update main.php

--- main/main.php
+++ main/main.php
@@ -1,5 +1,6 @@
 
 
-<div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-<div class="col-sm-12 id="mainDiv" table-responsive">
 
+
+
+HELLO WORLD

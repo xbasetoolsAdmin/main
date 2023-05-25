7e19dc24d9505d11dcc1a68c280bd4c66cb8fc62
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 02:28:44 +0100

    Update cpanels.php

--- main/cpanels.php
+++ main/cpanels.php
@@ -432,10 +432,9 @@ Hu<?php
     </div>
     
     <div style="padding-left:20px; display:inline-block; margin-bottom:10px">
-        <span><a href="addBalance.php?do=balance" class="btn btn-primary" style="color:#fff; font-size: 12 px">Add Balance</a></span>
-        <span><a href="addBalance.php?do=details" class="btn btn-primary" style="color:#fff; font-size: 12 px">Balance Info</a></span>
-    </div>
+
     <div id="mainDiv"></div>
+   
     <script type="text/javascript">
     $(document).keydown(function(event){
     if(event.which=="17")

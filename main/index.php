650645b1d3f1c6bd2569a08455d0c8372ca83dce
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 16:02:26 +0100

    Update index.php

--- main/index.php
+++ main/index.php
@@ -379,7 +379,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
             $('.dropdown').hover(function() {
                 $('.dropdown-toggle', this).trigger('click');
             });
-            pageDiv(13, 'Add Balance - OdinShop', 'addBalance', 1);
+            pageDiv(2, 'Add Balance - XBASETOOLS', '', 1);
             var clipboard = new Clipboard('.copyit');
             clipboard.on('success', function(e) {
                 setTooltip(e.trigger, 'Copied!');

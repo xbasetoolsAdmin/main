ede8a1d564a03020ea444fb95b896a65836e556e
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 06:19:59 -0700

    Update index.php

--- main/index.php
+++ main/index.php
@@ -81,13 +81,10 @@ monthly pageviews, Alexa Ranks , unique visitors, site revenue (from advertising
 <option value=""></option><option value="1">1</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="3">3</option><option value="30">30</option><option value="31">31</option><option value="32">32</option><option value="33">33</option><option value="34">34</option><option value="35">35</option><option value="36">36</option><option value="37">37</option><option value="38">38</option><option value="39">39</option><option value="4">4</option><option value="40">40</option><option value="42">42</option><option value="43">43</option><option value="44">44</option><option value="5">5</option><option value="50">50</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option> </select>
 </div>
 </div>
-	<?php
 
-	                                             require_once('main.php');
-	
-	?>
 <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-<div class="col-sm-12 table-responsive"><table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" id=mainDiv">
+<div class="col-sm-12 table-responsive">
+<table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" id="mainDiv">
 	<thead><tr><th data-priority="1">
 	</th><th class="all">ID</th>
 	<th data-priority="3">Country</th>

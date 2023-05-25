4c89db66e64625e5c18729333f147397e0a051b3
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 04:32:28 -0700

    Update p_header.php

--- main/p_header.php
+++ main/p_header.php
@@ -62,97 +62,280 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
             {
             margin:0 !important
             }
-        </style>
-<script type="text/javascript">
-             function ajaxinfo() {
-                $.ajax({
-                    type: 'GET',
-                    url: 'ajaxinfo.html',
-                    timeout: 10000,
-
-                    success: function(data) {
-                        if (data != '01') {
-                            var data = JSON.parse(data);
-                            for (var prop in data) {
-                                $("#" + prop).html(data[prop]).show();
-                            }
-                        } else {
-                            window.location = "logout.html";
-                        }
-                    }
-                });
-
-            }
-            setInterval(function() {
-                ajaxinfo()
-            }, 3000);
-
-            ajaxinfo();
-
-$(document).keydown(function(event){
-    if(event.which=="17")
-        cntrlIsPressed = true;
-});
-
-$(document).keyup(function(){
-    cntrlIsPressed = false;
-});
-
-var cntrlIsPressed = false;
+	</style>
+<style>
 
 
-function pageDiv(n,t,u,x){
-  if(cntrlIsPressed){
-    window.open(u, '_blank');
-    return false;
-  }
-        var obj = { Title: t, Url: u };
-        if ( ("/"+obj.Url) != location.pathname) {
-        	if (x != 1) {history.pushState(obj, obj.Title, obj.Url);}
-        	else{history.replaceState(obj, obj.Title, obj.Url);}
+  .panel-default {
+    border-color: #ecf0f1;
+}
 
-    	}
-      document.title = obj.Title;
-    $("#mainDiv").html('<div id="mydiv"><img src="/buyer/files/img/load2.gif" class="ajax-loader"></div>').show();
-    $.ajax({
-    type:       'GET',
-    url:        'divPage'+n+'.html',
-    success:    function(data)
+.panel {
+  background-color: var(--color-card);
+  border: 1px solid transparent;
+  border-radius: 4px;
+  -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+}
+  .card {
+       background-color: var(--color-card);
+  border: 1px solid transparent;
+  border-radius: 4px;
+  -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+    border-color: var(--color-card);
+}
+
+.panel-body {
+    padding: 15px;
+}
+
+.panel-footer {
+    padding: 10px 15px;
+    background-color: var(--color-secondary2);
+    border-top: 1px solid var(--color-secondary2);
+    border-bottom-right-radius: 3px;
+    border-bottom-left-radius: 3px;
+
+}
+
+.panel-footer .label-info {
+    background-color: #3498db;
+}
+
+.panel-footer .label-danger {
+    background-color: #e74c3c;
+}
+.panel-footer .label-warning {
+    background-color: #FFA726;
+}
+
+
+.panel-footer .label {
+
+    display: inline;
+    padding: .2em .6em .3em;
+    font-size: 100%;
+    font-weight: bold;
+    line-height: 1;
+
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25em;
+}
+.display  td {
+    background: var(--color-card);
+      color: var(--font-color);
+
+}
+.dataTables_wrapper .dataTables_paginate .paginate_button {
+
+    color: var(--font-color);
+
+}
+#table_paginate .paginate_button {
+color: var(--font-color);
+
+}
+
+.alert-info {
+    color: var(--color-info);
+    background-color: var(--color-backinfo);
+    border-color: var(--color-borderinfo);
+
+}
+
+#table_filter{
+  color: var(--font-color);
+}
+#table_length{
+  color: var(--font-color);
+}
+#table_paginate{
+  color: var(--font-color);
+}
+#table_info{
+  color: var(--font-color);
+}
+
+    </style>
+<body class="them">
+<style>
+    .navbar-nav .dropdown-menu
     {
-        $("#mainDiv").html(data).show();
-        newTableObject = document.getElementById('table');
-        sorttable.makeSortable(newTableObject);
-        $(".sticky-header").floatThead({top:60});
-        if(x==0){ajaxinfo();}
-      }});
-    if (typeof stopCheckBTC === 'function') { 
-    var a = stopCheckBTC();
-     }
+      margin:0 !important
+    }
+    .theme-light {
+  --color-primary: #0060df;
+  --color-secondary: #ffffff;
+   --color-secondary2: #ecf0f1;
+  --color-accent: #fd6f53;
+  --font-color: #000000;
+  --color-nav: #ffffff;
+  --color-dropdown: #ffffff;
+  --color-card: #ffffff;
+   --color-card2: #d1ecf1;
+  --color-info: #0c5460;
+  --color-backinfo: #d1ecf1;
+  --color-borderinfo: #bee5eb;
 
 }
+.theme-dark {
+  --color-primary: #17ed90;
+  --color-secondary: #353B50;
+  --color-secondary2: #353B50;
+  --color-accent: #12cdea;
+  --font-color: #ffffff;
+  --color-nav: #363947;
+  --color-dropdown: rgba(171, 205, 239, 0.3);
+  --color-card: #262A37;
+   --color-card2: #262A37;
+   --color-info: #4DD0E1;
+  --color-backinfo: #262A37;
+  --color-borderinfo: #262A37;
+}
+.them {
 
-$(window).on("popstate", function(e) {
-        location.replace(document.location);
-
-});
-
-
-function setTooltip(btn, message) {
-  console.log("hide-1");
-  $(btn).tooltip('hide')
-    .attr('data-original-title', message)
-    .tooltip('show');
-     console.log("show");
+  background: var(--color-secondary);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.them h1 {
+  color: var(--font-color);
+  font-family: sans-serif;
+}
+.card-body {
+     color: var(--font-color);
+    }
+.them button {
+  color: var(--font-color);
+  background-color: #ffffff;
+  padding: 10px 20px;
+  border: 0;
+  border-radius: 5px;
+}
+.navbar.navbar-light .navbar-toggler {
+    color: var(--font-color);
 }
 
-function hideTooltip(btn) {
-  setTimeout(function() {$(btn).tooltip('hide'); console.log("hide-2");}, 1000);
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
 }
-</script>
-	
-<body style="padding-top: 70px; padding-bottom: 70px;">
 
-<nav class="navbar navbar-default navbar-fixed-top ">
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 40px;
+  width: 40px;
+  left: 0px;
+  bottom: 4px;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  box-shadow: 0 0px 15px #2020203d;
+  background: white url('https://i.ibb.co/FxzBYR9/night.png');
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+input:checked + .slider {
+  background-color: #2196f3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(24px);
+  -ms-transform: translateX(24px);
+  transform: translateX(24px);
+  background: white url('https://i.ibb.co/7JfqXxB/sunny.png');
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+  </style>
+<script>
+
+        function setTheme(themeName) {
+            localStorage.setItem('theme', themeName);
+            document.documentElement.className = themeName;
+        }
+
+        // function to toggle between light and dark theme
+        function toggleTheme() {
+            if (localStorage.getItem('theme') === 'theme-dark') {
+                setTheme('theme-light');
+            } else {
+                setTheme('theme-dark');
+            }
+        }
+
+        // Immediately invoked function to set the theme on initial load
+        (function () {
+            if (localStorage.getItem('theme') === 'theme-dark') {
+                setTheme('theme-dark');
+                document.getElementById('slider').checked = false;
+            } else {
+                setTheme('theme-light');
+              document.getElementById('slider').checked = true;
+            }
+        })();
+
+  </script>
+<nav class="navbar navbar-expand-xl navbar  navbar-light " style="
+                                                          position:fixed;
+                                                          background-color: var(--color-nav);
+                                                          z-index:1;
+                                                          top:0;
+                                                          left:0;
+                                                          right:0;
+                                                          line-height: 1.5;
+                                                          font-family: 'Lato', sans-serif;
+                                                          font-size: 15px;
+                                                          padding-top: 0.5rem;
+                                                          padding-right: 1rem;
+                                                          padding-bottom: 0.5rem;
+                                                          padding-left: 1rem;
+                                                        ">
   <div class="container-fluid">
     <div class="navbar-header">
        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>

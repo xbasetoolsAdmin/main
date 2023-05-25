f15cf707173a33c769c3094bd72b184c569e89e9
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 20:09:10 +0100

    Update index.php

--- main/index.php
+++ main/index.php
@@ -41,30 +41,4 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
                 $('.dropdown-toggle', this).trigger('click');
             });
             pageDiv(1, 'Add Balance - OdinShop', 'main', 1);
-            var clipboard = new Clipboard('.copyit');
-            clipboard.on('success', function(e) {
-                setTooltip(e.trigger, 'Copied!');
-                hideTooltip(e.trigger);
-                e.clearSelection();
-            });
- 
-        });
- 
- 
-        function setTooltip(btn, message) {
-            //console.log("hide-1");
-            $(btn).tooltip('hide')
-                .attr('data-original-title', message)
-                .tooltip('show');
-            //console.log("show");
-        }
- 
-        function hideTooltip(btn) {
-            setTimeout(function() {
-                $(btn).tooltip('hide'); /*console.log("hide-2");*/
-            }, 1000);
-        }
-    </script>
-</body>
- 
-</html>
+

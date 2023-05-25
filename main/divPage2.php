cf1cceae43f322e419e76a370b57a39f0abe8f92
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 17:43:02 +0100

    Update divPage2.php

--- main/divPage2.php
+++ main/divPage2.php
@@ -84,7 +84,7 @@ monthly pageviews, Alexa Ranks , unique visitors, site revenue (from advertising
 
 <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
 <div class="col-sm-12 table-responsive">
-<table id="cpanal_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);"  id="mainDiv">
+<table id="cpanel_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);"  id="mainDiv">
                          <thead>
                            <tr>
         <th data-priority="1">0<th>

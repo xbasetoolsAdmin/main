48dfb301733524679584adc345c7d9f113b6c27e
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 17:06:15 +0100

    Update index.php

--- main/index.php
+++ main/index.php
@@ -19,11 +19,17 @@ include"category/cpanel_table.php";
 
 
 
+<div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+<div class="col-sm-12 table-responsive" id="mainDiv">
+<table id="cpanel_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
+<thead>
+    
+    
     </div>
     
 
     
-    
+
     <script>
             pageDiv(2, 'Add Balance - XBASETOOLS', '', 1);
 </script>

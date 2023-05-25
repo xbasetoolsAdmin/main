6b5e058aa02cbb501b4bf6effaf1fbae24e351b8
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 01:30:22 +0100

    Update cpanels.php

--- main/cpanels.php
+++ main/cpanels.php
@@ -1,113 +1,202 @@
-<?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");    
-    exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
+<?php
+   ob_start();
+   session_start();
+   date_default_timezone_set('UTC');
+   include "includes/config.php";
+   
+   if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
+       header("location: ../");
+       exit();
+   }
+   $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+   ?>
+<ul class="nav nav-tabs">
+   <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
+</ul>
+<div id="myTabContent" class="tab-content" >
+   <div class="tab-pane active in" id="filter">
+      <table class="table">
+         <thead>
+            <tr>
+               <th>Country</th>
+               <th>Domain TLD</th>
+               <th>Detected Hosting</th>
+               <th>Seller</th>
+               <th></th>
+            </tr>
+         </thead>
+         <tbody>
+            <tr>
+               <td>
+                  <select class='filterselect form-control input-sm' name="cpanel_country">
+                     <option value="">ALL</option>
+                     <?php
+                        $query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `cpanels` WHERE `sold` = '0' ORDER BY country ASC");
+                        	while($row = mysqli_fetch_assoc($query)){
+                        	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
+                        	}
+                        ?>
+                  </select>
+               </td>
+               <td><input class='filterinput form-control input-sm' name="cpanel_tld" size='3'></td>
+               <td><input class='filterinput form-control input-sm' name="cpanel_hosting" size='3'></td>
+               <td>  <select class='filterselect form-control input-sm' name="cpanel_seller">
+                     <option value="">ALL</option>
+                     <?php
+                        $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `cpanels` WHERE `sold` = '0' ORDER BY resseller ASC");
+                        	while($row = mysqli_fetch_assoc($query)){
+                        		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
+                        		   while($rpw = mysqli_fetch_assoc($qer))
+                        			 $SellerNick = "seller".$rpw["id"]."";
+                        	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
+                        	}
+                        ?>
+                  </select>
+               </td>
+               <td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td>
+            </tr>
+         </tbody>
+      </table>
+   </div>
+</div>
+<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
+   <thead>
+      <tr>
+         <th scope="col" >Country</th>
+         <th scope="col">TLD</th>
+         <th scope="col">Detect Hosting</th>
+         <th scope="col">Seller</th>
+         <th scope="col">Check</th>
+         <th scope="col">Price</th>
+         <th scope="col">Added on </th>
+         <th scope="col">Buy</th>
+      </tr>
+   </thead>
+      <?php
+         include("cr.php");
+            $q = mysqli_query($dbcon, "SELECT * FROM cpanels WHERE sold='0' ORDER BY RAND()")or die(mysql_error());
+           	function srl($item)
+         {
+         $item0 = $item;
+         $item1 = rtrim($item0);
+         $item2 = ltrim($item1);
+         return $item2;
+         } 
+         
+         while($row = mysqli_fetch_assoc($q)){
+         	 $countryfullname = $row['country'];
+          $code = array_search("$countryfullname", $countrycodes);
+         $countrycode = strtolower($code);
+         
+         $url = $row['url'];
+         	$d = explode("|", $url);
+         $urled = srl($d[0]);
+         
+         	  $tld = end(explode(".", parse_url($urled, PHP_URL_HOST))); 
+           $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
+            while($rpw = mysqli_fetch_assoc($qer))
+         	 $SellerNick = "seller".$rpw["id"]."";
+            echo "
+         <tr>    
+           <td id='cpanel_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
+             <td id='cpanel_tld'> .".$tld." </td>
+           <td id='cpanel_hosting'> ".htmlspecialchars($row['infos'])." </td>
+           <td id='cpanel_seller'> ".htmlspecialchars($SellerNick)."</td>
+         ";
+         echo '<td><span id="shop'.$row["id"].'" type="cpanel"><a onclick="javascript:check('.$row["id"].');" class="btn btn-info btn-xs"><font color=white>Check</font></a></span><center></td>';
+         echo "
+           <td> ".htmlspecialchars($row['price'])."</td>
+            <td> ".htmlspecialchars($row['date'])."</td>
+           ";
+         
+           echo '
+           <td>
+         <span id="cpanel'.$row['id'].'" title="buy" type="cpanel"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
+           </td>
+                   </tr>
+            ';
+         }
+         
+         ?>
+   </tbody>
+<table>
+   <tfoot>
+      <tr>
+         <th scope="col" >Country</th>
+         <th scope="col">TLD</th>
+         <th scope="col">Detect Hosting</th>
+         <th scope="col">Seller</th>
+         <th scope="col">Check</th>
+         <th scope="col">Price</th>
+         <th scope="col">Added on </th>
+         <th scope="col">Buy</th>
+      </tr>
+</table>
 
-    <div class="alert alert-info text-left" role="alert" style="margin: 15px;">
-        <ul>
-            <li>We Have Fresh E-mails Lists for Diffrents Countries ( You Can Use it for Spam ). </li>
-            <li>Our E-mails Lists Guarante for you Good results because most of them are from Shops and Big Sites .</li>
-            <li>Combo use for cracking (Brute forcing attack) not use for login to email.</li>
-            <li>There is <b> 0 </b> Available.</li>
-        </ul>
-    </div>
-    <input type=hidden id="type" name="type" value="5" />
-    <div class="row m-3 pt-1" style="color: var(--font-color);">
-        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-            <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Description:</label>
-            <input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
-        </div>
-        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-            <label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
-            <select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
-                <option value="">All Countries</option>
-            </select>
-        </div>
-        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-            <label for="seller" style="margin-bottom: 10px; margin-top: 5px">Seller :</label>
-            <select name="seller" id="seller" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
-               
-																 <option value="">All</option>
-            </select>
-        </div>
-    </div>
-
-    <div class="row m-2 pt-3" style="color: var(--font-color); background-color: var(--color-card);">
-         <div class="col-sm-12 table-responsive" id="mainDiv">
-             <table id="mainDiv" class="display responsive table-hover" style="width:100%" style="color: var(--font-color); background-color: var(--color-card);">        
-                <thead>
-                    <tr>
-                        <th data-priority="1"></th>
-                        <th class="all">ID</th>
-                        <th data-priority="3">Country</th>
-                        <th data-priority="6">Description</th>
-                        <th data-priority="7">Email N</th>
-                        <th data-priority="8">Seller</th>
-                        <th data-priority="2">Proof</th>
-                        <th data-priority="9">Price</th>
-                        <th data-priority="10">Added on </th>
-                        <th class="all">Buy</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
-    </div>
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <p class="heading" id="myModalHeader"></p>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true" class="white-text">×</span>
-                    </button>
-                </div>
-                <div class="modal-body" id="modelbody">
-                </div>
-                <div class="modal-footer justify-content-center">
-                    <a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">Close</a>
-                </div>
-            </div>
-        </div>
-    </div>
- 
-    <div class="modal fade" id="modalConfirmBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-sm modal-notify modal-info" role="document">
- 
-            <div class="modal-content text-center">
- 
-                <div class="modal-header d-flex justify-content-center">
-                    <p class="heading">Are you sure?</p>
-                </div>
- 
-                <div class="modal-body">
-                    <i class='fas fa-shopping-cart fa-4x animated rotateIn'></i>
-                </div>
- 
-                <div class="modal-footer flex-center">
-                    <a onClick='confirmbye()' class="btn btn-outline-info waves-effect" data-dismiss="modal">Yes</a>
-                    <a type="button" class="btn btn-info" data-dismiss="modal">No</a>
-                </div>
-            </div>
- 
-        </div>
-    </div>
- 
- 
-    <div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
-        <div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
- 
-            <div class="modal-content">
- 
-                <div class="modal-body">
-                    <div class="row d-flex justify-content-center align-items-center">
-                        <img src="layout/images/balance.png">
-                        <span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
-                        <a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
-                            <i class="fas fa-book ml-1 white-text"></i>
-                        </a>
-                        <a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No, thanks</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
- 
-
+<script type="text/javascript">
+   $('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#cpanel_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#cpanel_tld").text().toLowerCase() );var ck3 = $.trim( $(this).find("#cpanel_hosting").text().toLowerCase() );var ck4 = $.trim( $(this).find("#cpanel_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="cpanel_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="cpanel_tld"]').val().toLowerCase() );var val3 = $.trim( $('input[name="cpanel_hosting"]').val().toLowerCase() );var val4 = $.trim( $('select[name="cpanel_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || ck3.indexOf(val3)==-1 || (ck4 != val4 && val4 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
+   function buythistool(id){
+     bootbox.confirm("Are you sure?", function(result) {
+           if(result ==true){
+         $.ajax({
+        method:"GET",
+        url:"buytool.php?id="+id+"&t=cpanels",
+        dataType:"text",
+        success:function(data){
+            if(data.match(/<button/)){
+   		 $("#cpanel"+id).html(data).show();
+            }else{
+               bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
+            }
+        },
+      });
+          ;}
+     });
+   }
+   g:xcheck=0;
+   function check(id){   
+        if(xcheck > 1){
+       bootbox.alert("<b>Wait</b> - Other checking operation is executed!");
+     } else {
+       xcheck++;
+       var type = $("#shop"+id).attr('type')
+   	$("#shop"+id).html('Checking...').show();
+   	$.ajax({
+   	type: 		'GET',
+   	url: 		'CheckCpanel'+id+'.html',
+   	success:	function(data)
+   	{
+   		$("#shop"+id).html(data).show();
+   		xcheck--;
+   	}});
+   } }
+   
+   function openitem(order){
+     $("#myModalLabel").text('Order #'+order);
+     $('#myModal').modal('show');
+     $.ajax({
+       type:       'GET',
+       url:        'showOrder'+order+'.html',
+       success:    function(data)
+       {
+           $("#modelbody").html(data).show();
+       }});
+   
+   }
+   
+</script>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+   <div class="modal-dialog" role="document">
+      <div class="modal-content">
+         <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel"></h4>
+         </div>
+         <div class="modal-body" id="modelbody">
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+         </div>
+      </div>
+   </div>
+</div>

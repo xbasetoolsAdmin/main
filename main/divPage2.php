53888ec72c8d2f08efa42ff213b4c8c8f824c1b7
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 21:51:06 +0100

    Update divPage2.php

--- main/divPage2.php
+++ main/divPage2.php
@@ -7,3 +7,15 @@
 <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
 <div class="col-sm-12 table-responsive">
 <table id="cpanel_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
+<thead>
+<tr>
+<th data-priority="1"></th>
+<th class="all">ID</th>
+<th data-priority="3">Country</th>
+<th data-priority="8">Type</th>
+<th data-priority="15">TLD</th>
+<th data-priority="14">Domain</th>
+<th data-priority="11">Hosting</th>
+<th data-priority="12">CMS</th>
+<th class="all" style="width:9% !important;">Ip Blacklist</th>
+<th class="all">Seo Info</th>

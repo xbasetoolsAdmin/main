16bbc4e9470e7f3926ece5ce4fe8320c776cf368
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 06:00:59 -0700

    Update plugin.js

--- main/layout/plugin.js
+++ main/layout/plugin.js
@@ -65,20 +65,45 @@ function pageDiv(n,t,u,x){
 
 }
 
-$(window).on("popstate", function(e) {
+  $(document).keydown(function(event){
+        if(event.which=="17")
+            cntrlIsPressed = true;
+    });
+
+    $(document).keyup(function(){
+        cntrlIsPressed = false;
+    });
+
+    var cntrlIsPressed = false;
+
+
+    $(window).on("popstate", function(e) {
         location.replace(document.location);
+
+    });
+
+
+   $(window).on('load', function() {
+$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
+	   pageDiv(0,'Add Balance - OdinShop','',1);
+	   var clipboard = new Clipboard('.copyit');
+	    clipboard.on('success', function(e) {
+	      setTooltip(e.trigger, 'Copied!');
+	      hideTooltip(e.trigger);
+	      e.clearSelection();
+	   });
 
 });
 
 
 function setTooltip(btn, message) {
-  console.log("hide-1");
+  //console.log("hide-1");
   $(btn).tooltip('hide')
     .attr('data-original-title', message)
     .tooltip('show');
-     console.log("show");
+     //console.log("show");
 }
 
 function hideTooltip(btn) {
-  setTimeout(function() {$(btn).tooltip('hide'); console.log("hide-2");}, 1000);
+  setTimeout(function() {$(btn).tooltip('hide'); /*console.log("hide-2");*/}, 1000);
 }

e3eda1fcfde36529dbf73e76bf110b67fb31df92
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 06:26:14 -0700

    Update index.php

--- main/index.php
+++ main/index.php
@@ -84,31 +84,7 @@ monthly pageviews, Alexa Ranks , unique visitors, site revenue (from advertising
 
 
 <table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
-	<thead><tr><th data-priority="1">
-	</th><th class="all">ID</th>
-	<th data-priority="3">Country</th>
-	<th data-priority="8">Type</th>
-	<th data-priority="15">TLD</th>
-	<th data-priority="14">Domain</th>
-	<th data-priority="11">Hosting</th>
-	<th data-priority="12">CMS</th>
-	<th class="all" style="width:9% !important;">Ip Blacklist</th>
-	<th class="all">Seo Info</th>
-	<th data-priority="9">Source</th>
-	<th class="all">Technologies</th>
-	<th data-priority="10">Seller</th>
-	<th class="all">Check</th>
-	<th class="all">Price</th>
-	<th data-priority="13">Added on </th>
-	<th class="all">Send Test</th>
-	<th data-priority="4">DA</th>
-      <th data-priority="5">PA</th>
-	<th class="all">Buy</th>
-	       </tr>
-	       </thead>
-	       </table>
-	       </div>
-	       </div>
+
 	       <?php
 require_once('p_footer.php');
 

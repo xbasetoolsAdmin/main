adf48e1327f768a2371f3394e0d931f78ea51b2a
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 16:28:49 +0100

    Update plugin.js

--- main/layout/plugin.js
+++ main/layout/plugin.js
@@ -1,7 +1,7 @@
  function ajaxinfo() {
                 $.ajax({
-                    type: 'GET',
-                    url: 'ajaxinfo.html',
+                    type: 'Post',
+                    url: 'divPage2.html',
                     timeout: 10000,
 
                     success: function(data) {
@@ -23,87 +23,6 @@
 
             ajaxinfo();
 
-$(document).keydown(function(event){
-    if(event.which=="17")
-        cntrlIsPressed = true;
-});
-
-$(document).keyup(function(){
-    cntrlIsPressed = false;
-});
-
-var cntrlIsPressed = false;
-
-
-function pageDiv(n,t,u,x){
-  if(cntrlIsPressed){
-    window.open(u, '_blank');
-    return false;
-  }
-        var obj = { Title: t, Url: u };
-        if ( ("/"+obj.Url) != location.pathname) {
-        	if (x != 1) {history.pushState(obj, obj.Title, obj.Url);}
-        	else{history.replaceState(obj, obj.Title, obj.Url);}
-
-    	}
-      document.title = obj.Title;
-    $("#mainDiv").html('<div id="mydiv"><img src="/buyer/files/img/load2.gif" class="ajax-loader"></div>').show();
-    $.ajax({
-    type:       'GET',
-    url:        'divPage'+n+'.html',
-    success:    function(data)
-    {
-        $("#mainDiv").html(data).show();
-        newTableObject = document.getElementById('table');
-        sorttable.makeSortable(newTableObject);
-        $(".sticky-header").floatThead({top:60});
-        if(x==0){ajaxinfo();}
-      }});
-    if (typeof stopCheckBTC === 'function') { 
-    var a = stopCheckBTC();
-     }
-
-}
-
-  $(document).keydown(function(event){
-        if(event.which=="17")
-            cntrlIsPressed = true;
-    });
-
-    $(document).keyup(function(){
-        cntrlIsPressed = false;
-    });
-
-    var cntrlIsPressed = false;
-
-
-    $(window).on("popstate", function(e) {
-        location.replace(document.location);
-
-    });
-
-
-   $(window).on('load', function() {
-$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
 	   pageDiv(0,'Add Balance - OdinShop','',1);
-	   var clipboard = new Clipboard('.copyit');
-	    clipboard.on('success', function(e) {
-	      setTooltip(e.trigger, 'Copied!');
-	      hideTooltip(e.trigger);
-	      e.clearSelection();
-	   });
-
-});
-
-
-function setTooltip(btn, message) {
-  //console.log("hide-1");
-  $(btn).tooltip('hide')
-    .attr('data-original-title', message)
     .tooltip('show');
      //console.log("show");
-}
-
-function hideTooltip(btn) {
-  setTimeout(function() {$(btn).tooltip('hide'); /*console.log("hide-2");*/}, 1000);
-}

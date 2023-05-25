4805071bea148c73798dfd4c530a722d67ad03ea
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 17:31:24 +0100

    Update cpanel_table.php

--- main/category/cpanel_table.php
+++ main/category/cpanel_table.php
@@ -158,16 +158,24 @@ monthly pageviews, Alexa Ranks , unique visitors, site revenue (from advertising
 </div>
 
 <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-<div class="col-sm-12 table-responsive" id="mainDiv">
-<table id="cpanel_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
-<thead>
-  <tr>
-    <th data-priority="1">0<th>
+<div class="col-sm-12 table-responsive">
+<table id="cpanel_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);"  id="mainDiv">
+                         <thead>
+                           <tr>
+        <th data-priority="1">0<th>
         <th data-priority="1">2<th>
          <th data-priority="1">3<th>
         <th data-priority="1">4<th>
         <th data-priority="1">5<th>
         <th data-priority="1">6<th>
+        <th data-priority="1">7<th>
+         <th data-priority="1">8<th>
+          <th data-priority="1">9<th>
+         <th data-priority="1">10<th>
+                            </tr>
+                         <thody>
+                    </table>
+            
     </div>
     
 

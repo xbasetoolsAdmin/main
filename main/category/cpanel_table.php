9d24dfc5d72e2636ef4568b0d62c7d0196b771c6
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 17:13:08 +0100

    Update cpanel_table.php

--- main/category/cpanel_table.php
+++ main/category/cpanel_table.php
@@ -121,10 +121,13 @@ monthly pageviews, Alexa Ranks , unique visitors, site revenue (from advertising
 <div class="col-sm-12 table-responsive" id="mainDiv">
 <table id="cpanel_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
 <thead>
-    
-    
-    
-    
+  <tr>
+    <th data-priority="1">0<th>
+        <th data-priority="1">2<th>
+         <th data-priority="1">3<th>
+        <th data-priority="1">4<th>
+        <th data-priority="1">5<th>
+        <th data-priority="1">6<th>
     </div>
     
 

849497ea72e7467eea18da4030413d1cb563162b
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 11:45:24 +0100

    Update p_header.php

--- main/p_header.php
+++ main/p_header.php
@@ -14,16 +14,16 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
-<link rel="stylesheet" type="text/css" href="files/css/flags.css" />
+<link rel="stylesheet" type="text/css" href="../files/css/flags.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
-<script type="text/javascript" src="files/js/jquery.js?1"></script>
-<script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
-<script type="text/javascript" src="files/js/sorttable.js"></script>
-<script type="text/javascript" src="files/js/table-head.js?3334"></script>
-<script type="text/javascript" src="files/js/bootbox.min.js"></script>
-<script type="text/javascript" src="files/js/clipboard.min.js"></script>
+<script type="text/javascript" src="../files/js/jquery.js?1"></script>
+<script type="text/javascript" src="../files/bootstrap/3/js/bootstrap.js?1"></script>
+<script type="text/javascript" src="../files/js/sorttable.js"></script>
+<script type="text/javascript" src="../files/js/table-head.js?3334"></script>
+<script type="text/javascript" src="../files/js/bootbox.min.js"></script>
+<script type="text/javascript" src="../files/js/clipboard.min.js"></script>
 
-<link rel="shortcut icon" href="files/img/favicon.ico" />
+<link rel="shortcut icon" href="../files/img/favicon.ico" />
 <meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> 
  <meta name="referrer" content="no-referrer" />
 
@@ -125,7 +125,7 @@ function pageDiv(n,t,u,x){
 
     	}
       document.title = obj.Title;
-    $("#mainDiv").html('<div id="mydiv"><img src="files/img/load2.gif" class="ajax-loader"></div>').show();
+    $("#mainDiv").html('<div id="mydiv"><img src="../files/img/load2.gif" class="ajax-loader"></div>').show();
     $.ajax({
     type:       'GET',
     url:        'divPage'+n+'.html',
